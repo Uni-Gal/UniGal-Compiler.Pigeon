d4c47e00a56950bf1eaa15bfd4ec22f18e5aa8b0
Fa鸽 <1207247008@qq.com>
2021-02-02 01:32:10 +0800

    Runtime一节

--- UniGal.Compiler.IR/RuntimeEnvironment.cs
+++ UniGal.Compiler.IR/RuntimeEnvironment.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +8,59 @@ namespace UniGal.Compiler.IR
 {
 	public class RuntimeEnvironment : BasicElement
 	{
+		/// <summary>
+		/// 硬编码的显示设定
+		/// </summary>
+		public struct Display
+		{
+			/// <summary>宽，以像素为单位</summary>
+			public uint Width;
+			/// <summary>高，以像素为单位</summary>
+			public uint Height;
+			/// <summary>是不是全屏显示</summary>
+			public bool FullScreen;
 
+			public Display(uint w, uint h, bool fullscreen)
+			{
+				Width = w;
+				Height = h;
+				FullScreen = fullscreen;
+			}
+		}
+
+		/// <summary>需要安装的再分发包</summary>
+		public record RedistPackage
+		{
+			#pragma warning disable CS1591
+			public string Name = "";
+			public string Version = "";
+			#pragma warning restore
+
+			/// <summary>
+			/// 确认该再分发包的值是否有效
+			/// </summary>
+			/// <returns></returns>
+			public bool IsValid()
+			{
+				return Name != "" && Version != "";
+			}
+		}
+
+		/// <summary>
+		/// 显示设定
+		/// </summary>
+		public readonly Display DisplaySettings;
+		/// <summary>需要安装的再分发包</summary>
+		public IEnumerable<RedistPackage> Redists { get; init; }
+
+		public RuntimeEnvironment(Display displaySettings, IEnumerable<RedistPackage> redists)
+		{
+			DisplaySettings = displaySettings;
+			Redists = redists;
+		}
+		public RuntimeEnvironment(uint w, uint h, bool fullscreen, IEnumerable<RedistPackage> redists) : this(new(w,h,fullscreen),redists)
+		{
+
+		}
 	}
 }
